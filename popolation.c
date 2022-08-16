889ef421475f4eda8dad255dc6ddc7d5552b7a6a
Jaylson Almeida <may2almeida@gmail.com>
2022-08-16 19:03:24 -0100

    new C program

--- popolation.c
+++ popolation.c
@@ -4,6 +4,8 @@
 int main(void)
 {
     int n, f, year;
+    
+    year=0;
     do
     {
        // TODO: Prompt for start size
@@ -11,15 +13,28 @@ int main(void)
         scanf("%d",&n);
 
     // TODO: Prompt for end size
-        printf("Populacao Final: ")
-        scanf("%d",&f)
-    } while (n>9 && f>=n);
+        printf("Populacao Final: ");
+        scanf("%d",&f);
+    } while (n<9 || f<=n);
+   
+    int population=n;
 
-
-    
-    
     
     // TODO: Calculate number of years until we reach threshold
+    while (population<f)
+    {
+        int birth=population/3;      //calc new poplt
+        int death=population/4;      //deat population
+        population=population+birth-death; //new popopl value
+        year++;  
+    }
+
+    printf("has pass: %d",year);
+    
+
+    
+
+
 
     // TODO: Print number of years
 }
